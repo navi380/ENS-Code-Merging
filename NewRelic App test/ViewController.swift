34a03099070fa693fcea1a08c4fb5cf82ff2d2e3
hamza.salar <hamza.salar@evampsaanga.com>
2022-08-30 15:29:59 +0500

    test

--- NewRelic App test/ViewController.swift
+++ NewRelic App test/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Hello world")
         print("this is naveed.")
+        
+        print("this is hello.")
     }
 
 
